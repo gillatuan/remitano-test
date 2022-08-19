8a57f418e7e6b13378ee2ea7caa1cf77b4f3832f
TuanBui <tuan.bui@propzy.com>
2022-08-19 17:06:31 +0700

    render video info component

--- src/components/ShareVideo/index.js
+++ src/components/ShareVideo/index.js
@@ -1,10 +1,27 @@
+import { Col, Row } from "react-bootstrap"
+
 export const ShareVideo = (props) => {
   const { item } = props
 
   return (
-    <>
-      <p>{item.title}</p>
-      <p>Share by: {item.username}</p>
-    </>
+    <Row>
+      <Col md={6}>
+        <iframe
+          width="100%"
+          height="180"
+          src={item.author_url}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </Col>
+
+      <Col md={6}>
+        <p>{item.title}</p>
+        <p>Share by: {item.username}</p>
+        <p></p>
+      </Col>
+    </Row>
   )
 }
